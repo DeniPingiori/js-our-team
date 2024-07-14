3381d46072448d3b3bbd8ac188cbb99039a10ffd
Denise Pingiori <pingiorid@gmail.com>
2024-07-14 12:37:08 +0200

    [upd] upd array teams

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
+//array teams
 const teams = [
     {
-        name:"Wayne Barnett",  role:"Founder & CEO", role: "wayne-barnett-founder-ceo.jpg"
-        name:"Angela Caroll", role:"Chief Editor", role:"angela-caroll-chief-editor.jpg"
-        name:"Walter Gordon", role:"Office Manager", role:" walter-gordon-office-manager.jpg"
-        name:"Angela Lopez", role:"Social Media Manager", role:"angela-lopez-social-media-manager.jpg"
-        name:"Scott Estrada", role:"Developer", role:"scott-estrada-developer.jpg"
-        name:"Barbara Ramos", role:" Graphic Desinerbarbara", role:"ramos-graphic-desiner.jpg"
-    }
-]
+        name: "Wayne Barnett",  
+        role: "Founder & CEO", 
+        img: "wayne-barnett-founder-ceo.jpg"
+    }, 
+    {
+        name: "Angela Caroll", 
+        role: "Chief Editor", 
+        img: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager", 
+        img:  "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez", 
+        role: "Social Media Manager",
+        img : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada", 
+        role: "Developer", 
+        img: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: " Graphic Desinerbarbara", 
+        img: "ramos-graphic-desiner.jpg"
+    },
+];
 
