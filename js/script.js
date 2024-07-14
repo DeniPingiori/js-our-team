5b5e6194c510b1148c3423b64bc1764d374f09c2
Denise Pingiori <pingiorid@gmail.com>
2024-07-14 12:30:37 +0200

    [upd] upd array

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const teams = [
+    {
+        name:"Wayne Barnett",  role:"Founder & CEO", role: "wayne-barnett-founder-ceo.jpg"
+        name:"Angela Caroll", role:"Chief Editor", role:"angela-caroll-chief-editor.jpg"
+        name:"Walter Gordon", role:"Office Manager", role:" walter-gordon-office-manager.jpg"
+        name:"Angela Lopez", role:"Social Media Manager", role:"angela-lopez-social-media-manager.jpg"
+        name:"Scott Estrada", role:"Developer", role:"scott-estrada-developer.jpg"
+        name:"Barbara Ramos", role:" Graphic Desinerbarbara", role:"ramos-graphic-desiner.jpg"
+    }
+]
+
